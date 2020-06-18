633f6cbd67a7867b501575dd968761c29eb572a2
suvam prasad <tituprasad30@gmail.com>
2020-06-18 12:44:17 +0530

    lesson 2 completed

--- lib/main.dart
+++ lib/main.dart
@@ -16,8 +16,37 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: Text('Awesome App'),
       ),
-      body: Container(
-        child: Text('Hi Awesome App'),
+      body: Center(
+        child: Container(
+          padding: const EdgeInsets.all(8),
+          width: 100,
+          height: 100,
+          clipBehavior: Clip.antiAlias,
+          alignment: Alignment.center,
+          decoration: BoxDecoration(
+            color: Colors.red, borderRadius: BorderRadius.circular(8),
+            gradient: LinearGradient(colors: [
+              Colors.red,
+              Colors.yellow,
+            ]),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.grey,
+                blurRadius: 50,
+              ),
+            ],
+            // shape: BoxShape.circle,
+          ),
+          child: Text(
+            'Hi Awesome App',
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 15,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
       ),
     );
   }
