35a044d61622e4892bae090f1ecfc9f75cd17889
suvam prasad <tituprasad30@gmail.com>
2020-06-18 12:57:11 +0530

    lesson 3 completed

--- lib/main.dart
+++ lib/main.dart
@@ -16,36 +16,33 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: Text('Awesome App'),
       ),
-      body: Center(
-        child: Container(
-          padding: const EdgeInsets.all(8),
-          width: 100,
-          height: 100,
-          clipBehavior: Clip.antiAlias,
-          alignment: Alignment.center,
-          decoration: BoxDecoration(
-            color: Colors.red, borderRadius: BorderRadius.circular(8),
-            gradient: LinearGradient(colors: [
-              Colors.red,
-              Colors.yellow,
-            ]),
-            boxShadow: [
-              BoxShadow(
-                color: Colors.grey,
-                blurRadius: 50,
-              ),
-            ],
-            // shape: BoxShape.circle,
-          ),
-          child: Text(
-            'Hi Awesome App',
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              color: Colors.white,
-              fontSize: 15,
-              fontWeight: FontWeight.bold,
+      body: Container(
+        color: Colors.teal,
+        width: 500,
+        height: 400,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Container(
+              padding: const EdgeInsets.all(8),
+              width: 100,
+              height: 100,
+              color: Colors.red,
             ),
-          ),
+            Container(
+              padding: const EdgeInsets.all(8),
+              width: 100,
+              height: 100,
+              color: Colors.yellow,
+            ),
+            Container(
+              padding: const EdgeInsets.all(8),
+              width: 100,
+              height: 100,
+              color: Colors.green,
+            ),
+          ],
         ),
       ),
     );
