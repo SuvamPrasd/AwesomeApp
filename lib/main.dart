e78aad898e87a592f1492f879e8cad69afc92123
suvam prasad <tituprasad30@gmail.com>
2020-06-18 13:24:24 +0530

    lesson 4 completed

--- lib/main.dart
+++ lib/main.dart
@@ -16,34 +16,42 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: Text('Awesome App'),
       ),
-      body: Container(
-        color: Colors.teal,
-        width: 500,
-        height: 400,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
+      body: Center(
+        child: Container(
+          color: Colors.teal,
+          width: 100,
+          height: 100,
+        ),
+      ),
+      drawer: Drawer(
+        child: ListView(
           children: <Widget>[
-            Container(
-              padding: const EdgeInsets.all(8),
-              width: 100,
-              height: 100,
-              color: Colors.red,
+            UserAccountsDrawerHeader(
+              accountName: Text('Suvam Prasad'),
+              accountEmail: Text('suvamprasad5@gmail.com'),
+              currentAccountPicture: CircleAvatar(
+                backgroundImage: NetworkImage(
+                    'https://images.unsplash.com/photo-1553798194-cc0213ae7f99?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=751&q=80'),
+              ),
             ),
-            Container(
-              padding: const EdgeInsets.all(8),
-              width: 100,
-              height: 100,
-              color: Colors.yellow,
+            ListTile(
+              leading: Icon(Icons.person),
+              title: Text('Suvam Prasad'),
+              subtitle: Text('Developer'),
+              trailing: Icon(Icons.edit),
             ),
-            Container(
-              padding: const EdgeInsets.all(8),
-              width: 100,
-              height: 100,
-              color: Colors.green,
+            ListTile(
+              leading: Icon(Icons.email),
+              title: Text('Contact Me'),
+              subtitle: Text('suvamprasad5@gmail.com'),
+              trailing: Icon(Icons.edit),
             ),
           ],
         ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(Icons.edit),
       ),
     );
   }
